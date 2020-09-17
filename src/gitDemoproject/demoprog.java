c194b63b85cd42bfc28076c0b771f120481838cc
PRADNYA <PRADNYA@PRADNYA-PC>
2020-09-17 05:32:07 -0700

    first commit

---
+++ src/gitDemoproject/demoprog.java
@@ -0,0 +1,11 @@
+package gitDemoproject;
+
+public class demoprog {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello program");
+
+	}
+
+}
